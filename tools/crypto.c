6e22c8f3536b23a03ad35f5f9eaf545f01e35567
Sam and Alex <totesanemail@andrew.cmu.edu>
2019-02-21 22:47:23 +0000

    C file now decrypts and verifies fully encrypted game and header

--- tools/crypto.c
+++ tools/crypto.c
@@ -1,7 +1,8 @@
 #include <sodium.h>
 #include <stdio.h>
 #include <string.h>
-//#include "libsodium.h"
+/*
+#include "libsodium.h"
 
 #define crypto_pwhash_SALTBYTES 16U
 #define crypto_pwhash_OPSLIMIT_MIN 1U
@@ -11,7 +12,7 @@
 #define crypto_secretbox_NONCEBYTES 24U
 #define crypto_secretbox_KEYBYTES 32U
 #define crypto_sign_PUBLICKEYBYTES 32U
-
+*/
 #define HEADER_LEN 16
 
 unsigned int get_len(char *file_name){
@@ -279,8 +280,179 @@ void gen_userkey_test(){
     gen_userkey(user_key,"user1", "12345678", "2048", "1.1");
 }
 
+void full_decrypt_test(){
+    // read in pulbic key, header key and nonce, salt for a user(the test user)
+    // read in file
+    // verify signature
+    // get header size
+    // split header and game
+    // decrypt header
+    // verify name, version
+    // Get E(gamekey_nonce) and nonce for the specified user
+    // decrypt gamekey nonce
+    // split key and nonce
+    // decrypt game
+    // output game to a file
+    char *user = "user";
+    char *pin = "56781234";
+    char *name = "2048";
+    char *version = "1.1"; // Use 1.1, since the salt will get overwritten from 1.0
+    char *signed_ciphertext;
+    char user_key[crypto_secretbox_KEYBYTES];
+    char user_nonce[crypto_secretbox_NONCEBYTES];
+    char key[crypto_secretbox_KEYBYTES];
+    char nonce[crypto_secretbox_NONCEBYTES];
+    char pk[crypto_sign_PUBLICKEYBYTES]; 
+    char gamekey[crypto_secretbox_KEYBYTES];
+    char gamenonce[crypto_secretbox_NONCEBYTES];
+    // These vaues are ONLY for this test game 2048
+    char *game_name;
+    char *game_version;
+    // This will change is we stop using base64
+    char *encoded_gamekeynonce;
+    char *encoded_nonce;
+    char *verified_ciphertext;
+    char *encrypted_header;
+    char *decrypted_header;
+    unsigned long long int unverified_len;
+    unsigned long long int verified_len;
+    unsigned long long int encrypted_header_len;
+    unsigned long long int decrypted_header_len;
+    unsigned long long int encrypted_game_len;
+    unsigned long long int decrypted_game_len;
+
+    if (sodium_init() < 0) {
+        printf("Error in Crypto Library\n");
+        exit(0);
+    }
+    // This function reads the salt in from salt.out
+    gen_userkey(user_key, user, pin, name, version);
+    // read the key
+    read_from_file(key, "key.out", crypto_secretbox_KEYBYTES);
+    // read the nonce.
+    read_from_file(nonce, "nonce.out", crypto_secretbox_NONCEBYTES);
+    // read the public key
+    read_from_file(pk, "pk.out", crypto_sign_PUBLICKEYBYTES);
+    // Get the length of the file 
+    unverified_len = get_len("2048-v1.1");
+    // Read in the game file
+    signed_ciphertext = safe_calloc(unverified_len);
+    read_from_file(signed_ciphertext, "2048-v1.1", unverified_len);
+    verified_len = unverified_len -  crypto_sign_BYTES;
+    verified_ciphertext = safe_calloc(verified_len);
+    // Check the signature
+    if(verify_signed(signed_ciphertext, verified_ciphertext, unverified_len, pk) == 0){    
+        // Read in the size of the encrypted header. 
+        memcpy(&encrypted_header_len, verified_ciphertext, sizeof(unsigned long long int));
+        decrypted_header_len = encrypted_header_len - crypto_secretbox_MACBYTES;
+        encrypted_game_len = verified_len - encrypted_header_len - sizeof(unsigned long long int);
+        decrypted_game_len = encrypted_game_len - crypto_secretbox_MACBYTES;
+        // split header and game
+        encrypted_header = safe_calloc(encrypted_header_len);
+        memcpy(encrypted_header, verified_ciphertext + sizeof(unsigned long long int), encrypted_header_len);
+        // decrypt header 
+        decrypted_header = safe_calloc(decrypted_header_len); 
+        decrypt(key, nonce, encrypted_header, encrypted_header_len, decrypted_header);
+        strsep(&decrypted_header,":");
+        game_version = strsep(&decrypted_header,"\n");
+        strsep(&decrypted_header,":");
+        game_name = strsep(&decrypted_header,"\n");
+
+        printf("Version: %s\n", game_version);
+        printf("Name: %s\n", game_name);
+        // TODO: change to NOT use base64
+        char *row;
+        char *test_user;
+        // This may read one too many and crash!!! 
+        // TODO: fix this crash
+        int flag = 0;
+        while( (row = strsep(&decrypted_header,":")) != NULL ){
+            printf("Row: %s\n", row);
+            // read the first user
+            test_user = strsep(&decrypted_header," ");
+            printf("Test user: %s\n", test_user);
+            if(strcmp(test_user, user) == 0){
+                printf("Found the correct user\n");
+                encoded_gamekeynonce = strsep(&decrypted_header, " ");
+                encoded_nonce = strsep(&decrypted_header, "\n");
+                printf("encoded gamekey nonce: %s\n", encoded_gamekeynonce);
+                printf("encoded nonce: %s\n", encoded_nonce);
+                flag = 1;
+                break;
+            } else {
+                printf("Not correct user\n");
+                // strsep to the end of the line
+                strsep(&decrypted_header,"\n");
+            }
+        }
+        if (flag == 0){
+            printf("Not a valid user for this game!\n");
+            return;
+        }
+        
+        // All this BS is nessasary to covert from b'' format from python
+        encoded_gamekeynonce += 2;
+        encoded_nonce += 2;
+        encoded_gamekeynonce[strlen(encoded_gamekeynonce) - 1] = '\0'; 
+        encoded_nonce[strlen(encoded_nonce) - 1] = '\0'; 
+        printf("encoded gamekey nonce: %s\n", encoded_gamekeynonce);
+        printf("encoded nonce: %s\n", encoded_nonce);
+        // int sodium_base642bin(unsigned char * const bin, const size_t bin_maxlen,
+        //             const char * const b64, const size_t b64_len,
+        //             const char * const ignore, size_t * const bin_len,
+        //             const char ** const b64_end, const int variant); 
+		size_t const encrypted_gamekeynonce_len = crypto_secretbox_KEYBYTES + crypto_secretbox_NONCEBYTES + crypto_secretbox_MACBYTES;		
+        size_t const nonce_len = crypto_secretbox_NONCEBYTES;
+        unsigned char encrypted_gamekeynonce[encrypted_gamekeynonce_len];
+        size_t encoded_gamekeynonce_len = strlen(encoded_gamekeynonce);
+        size_t encoded_nonce_len = strlen(encoded_nonce);
+
+        sodium_base642bin(encrypted_gamekeynonce, sodium_base64_ENCODED_LEN(encrypted_gamekeynonce_len, sodium_base64_VARIANT_ORIGINAL),
+						encoded_gamekeynonce, encoded_gamekeynonce_len,
+						"", (size_t * const)&encrypted_gamekeynonce_len,
+						NULL, sodium_base64_VARIANT_ORIGINAL);
+		
+        sodium_base642bin(user_nonce, sodium_base64_ENCODED_LEN(crypto_secretbox_NONCEBYTES, sodium_base64_VARIANT_ORIGINAL),
+						encoded_nonce, encoded_nonce_len,
+						"", (size_t * const)&nonce_len,
+						NULL, sodium_base64_VARIANT_ORIGINAL);
+        // END CHANGE AFTER BASE64 IS REMOVED
+
+        char *gamekey_nonce;
+        gamekey_nonce = safe_calloc(encrypted_gamekeynonce_len - crypto_secretbox_MACBYTES);
+        // decrypt the gamekeynonce
+        decrypt(user_key, user_nonce, encrypted_gamekeynonce, encrypted_gamekeynonce_len, gamekey_nonce);
+        memcpy(gamekey, gamekey_nonce, crypto_secretbox_KEYBYTES);
+        memcpy(gamenonce, gamekey_nonce + crypto_secretbox_KEYBYTES, crypto_secretbox_NONCEBYTES);
+    
+        // decrypt game
+        char *message;
+        message = safe_calloc(decrypted_game_len);
+        char *encrypted_game;
+        encrypted_game = safe_calloc(encrypted_game_len);
+        memcpy(encrypted_game, verified_ciphertext + sizeof(unsigned long long int) + encrypted_header_len, encrypted_game_len);
+        decrypt(gamekey, gamenonce, encrypted_game, encrypted_game_len, message);
+        //printf("The message is %s\n", message);
+        FILE *fp;
+        fp = fopen("out.out", "w");
+        fwrite(message, 1, decrypted_game_len, fp);
+        fclose(fp);
+        safe_free(encrypted_header, encrypted_header_len);
+        safe_free(gamekey_nonce, decrypted_header_len);
+        safe_free(message, decrypted_game_len);
+        safe_free(encrypted_game, encrypted_game_len);
+        
+    } else {
+        printf("Sign verify failed\n");
+        // exit horribly
+    }
+    safe_free(signed_ciphertext, unverified_len);
+    safe_free(verified_ciphertext, verified_len);
+}
+
 int main(){
     // decrypt_test();
     // singed_basic_header_test();
-    gen_userkey_test();
+    // gen_userkey_test();
+    full_decrypt_test();
 }
