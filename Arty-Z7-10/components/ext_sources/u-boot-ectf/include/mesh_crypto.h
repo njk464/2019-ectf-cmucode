2cc6faff217d69e94c2acf324648692166862852
Alex Fulton <al3x.fulton@gmail.com>
2019-02-23 17:13:09 -0500

    add mesh_crypto in correct location

---
+++ Arty-Z7-10/components/ext_sources/u-boot-ectf/include/mesh_crypto.h
@@ -0,0 +1,11 @@
+//#include <sodium.h>
+#include <stdio.h>
+#include <string.h>
+#include <sodium.h>
+#include <secret.h>
+
+
+void *safe_malloc(size_t size);
+void safe_free(void* ptr, size_t size);
+int verify_user_can_play(char *username, char* pin, char* gamepath);
+void decrypt_game_file(char *username, char* pin, char* gamepath);
