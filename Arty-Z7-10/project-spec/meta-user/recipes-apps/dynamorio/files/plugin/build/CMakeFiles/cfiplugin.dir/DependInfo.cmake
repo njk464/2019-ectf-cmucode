899e3a0442acb81340d3b5083e5e59e660b09fb3
Wai Tuck Wong <wongwaituck@gmail.com>
2019-02-25 20:23:05 +0000

    added source files for cfiplugin

---
+++ Arty-Z7-10/project-spec/meta-user/recipes-apps/dynamorio/files/plugin/build/CMakeFiles/cfiplugin.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ubuntu/dynamorio_bin/plugin/cfi_plugin.c" "/home/ubuntu/dynamorio_bin/plugin/build/CMakeFiles/cfiplugin.dir/cfi_plugin.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/ubuntu/dynamorio_bin/DynamoRIO-ARM-Linux-EABIHF-7.1.0-1/cmake/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
